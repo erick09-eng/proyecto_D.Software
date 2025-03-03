02295b1a926ecbc0cbeee452c1aa400a16cf165c
erick bolivar <esbarboleda789@gmail.com>
2025-03-02 22:01:04 -0500

    commit inicial

---
+++ src/main/java/com/mycompany/supermarcket_dsoftware/SuperMarcket_dsoftware.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.supermarcket_dsoftware;
+
+/**
+ *
+ * @author parciball
+ */
+public class SuperMarcket_dsoftware {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
