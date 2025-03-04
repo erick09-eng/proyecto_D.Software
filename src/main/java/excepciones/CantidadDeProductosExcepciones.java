098a53dd722839ec18ad7ee5e7f5ffc2e9ec3ed2
Sebastian Munoz <sebastianmunozchaar@gmail.com>
2025-03-04 00:32:50 -0500

    se agregan excepciones

---
+++ src/main/java/excepciones/CantidadDeProductosExcepciones.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package excepciones;
+
+/**
+ *
+ * @author ANDRES FELIPE
+ */
+public class CantidadDeProductosExcepciones extends Exception{
+    public CantidadDeProductosExcepciones(String mensaje) {
+        super(mensaje);
+    }
+}
